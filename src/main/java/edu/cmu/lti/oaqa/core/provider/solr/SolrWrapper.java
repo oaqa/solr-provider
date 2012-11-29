93f2fabb6485e844b14309eb16cd2eabe9abdd86
alkesh <alkeshku@andrew.cmu.edu>
2012-11-28 20:25:17 -0500

    Uncommented local running of solr server code

--- src/main/java/edu/cmu/lti/oaqa/core/provider/solr/SolrWrapper.java
+++ src/main/java/edu/cmu/lti/oaqa/core/provider/solr/SolrWrapper.java
@@ -34,98 +34,102 @@ import org.apache.solr.core.CoreContainer;
 
 public final class SolrWrapper implements Closeable {
 
-  private static final String LOCALHOST = "localhost";
+	private static final String LOCALHOST = "localhost";
 
-  private final SolrServer server;
+	private final SolrServer server;
 
-  boolean embedded;
+	boolean embedded;
 
-  public SolrWrapper(String serverUrl, Integer serverPort, Boolean embedded, String core)
-          throws Exception {
-    if (embedded != null && embedded.booleanValue()) {
-      this.server = createEmbeddedSolrServer(core);
-      this.embedded = true;
-    } else {
-      /*if (canRunInLocalMode(serverUrl)) {
-        System.err.printf("Running Solr retrieval on local mode\n");
-        this.server = createSolrServer(LOCALHOST);
-      } else {*/
-        System.err.printf("Running Solr retrieval on remote mode\n");
-        this.server = createSolrServer(serverUrl);
-      //}
-    }
-  }
+	public SolrWrapper(String serverUrl, Integer serverPort, Boolean embedded,
+			String core) throws Exception {
+		if (embedded != null && embedded.booleanValue()) {
+			this.server = createEmbeddedSolrServer(core);
+			this.embedded = true;
+		} else {
+			if (canRunInLocalMode(serverUrl)) {
+				System.err.printf("Running Solr retrieval on local mode\n");
+				this.server = createSolrServer(LOCALHOST);
+			} else {
+				System.err.printf("Running Solr retrieval on remote mode\n");
+				this.server = createSolrServer(serverUrl);
+			}
+		}
+	}
 
-  private boolean canRunInLocalMode(String serverUrl) {
-    try {
-      URL url = new URL(serverUrl);
-      int port = url.getPort();
-      Socket socket = new Socket("localhost", port);
-      socket.close();
-      return true;
-    } catch (IOException e) {
-      return false;
-    }
-  }
+	private boolean canRunInLocalMode(String serverUrl) {
+		try {
+			URL url = new URL(serverUrl);
+			int port = url.getPort();
+			Socket socket = new Socket("localhost", port);
+			socket.close();
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
 
-  private SolrServer createSolrServer(String url) throws Exception {
-    SolrServer server = new HttpSolrServer(url);
-    server.ping();
-    return server;
-  }
+	private SolrServer createSolrServer(String url) throws Exception {
+		SolrServer server = new HttpSolrServer(url);
+		server.ping();
+		return server;
+	}
 
-  public SolrServer getServer() throws Exception{
-	  return server;
-  }
-  private SolrServer createEmbeddedSolrServer(String core) throws Exception {
-    System.setProperty("solr.solr.home", core);
-    CoreContainer.Initializer initializer = new CoreContainer.Initializer();
-    CoreContainer coreContainer = initializer.initialize();
-    return new EmbeddedSolrServer(coreContainer, "");
-  }
+	public SolrServer getServer() throws Exception {
+		return server;
+	}
 
-  public SolrDocumentList runQuery(String q, int results) throws SolrServerException {
-    SolrQuery query = new SolrQuery();
-    query.setQuery(escapeQuery(q));
-    query.setRows(results);
-    query.setFields("*", "score");
-    QueryResponse rsp = server.query(query,METHOD.POST);
-    return rsp.getResults();
-  }
+	private SolrServer createEmbeddedSolrServer(String core) throws Exception {
+		System.setProperty("solr.solr.home", core);
+		CoreContainer.Initializer initializer = new CoreContainer.Initializer();
+		CoreContainer coreContainer = initializer.initialize();
+		return new EmbeddedSolrServer(coreContainer, "");
+	}
 
-  public SolrDocumentList runQuery(SolrQuery query, int results) throws SolrServerException {
-    QueryResponse rsp = server.query(query);
-    return rsp.getResults();
-  }
+	public SolrDocumentList runQuery(String q, int results)
+			throws SolrServerException {
+		SolrQuery query = new SolrQuery();
+		query.setQuery(escapeQuery(q));
+		query.setRows(results);
+		query.setFields("*", "score");
+		QueryResponse rsp = server.query(query, METHOD.POST);
+		return rsp.getResults();
+	}
 
-  public String getDocText(String id) throws SolrServerException {
-    String q = "id:" + id;
-    SolrQuery query = new SolrQuery();
-    query.setQuery(q);
-    query.setFields("text");
-    QueryResponse rsp = server.query(query);
+	public SolrDocumentList runQuery(SolrQuery query, int results)
+			throws SolrServerException {
+		QueryResponse rsp = server.query(query);
+		return rsp.getResults();
+	}
 
-    String docText = "";
-    if (rsp.getResults().getNumFound() > 0) {
-      @SuppressWarnings({ "unchecked", "rawtypes" })
-      ArrayList<String> results = (ArrayList) rsp.getResults().get(0).getFieldValues("text");
-      docText = results.get(0);
-    }
-    return docText;
-  }
+	public String getDocText(String id) throws SolrServerException {
+		String q = "id:" + id;
+		SolrQuery query = new SolrQuery();
+		query.setQuery(q);
+		query.setFields("text");
+		QueryResponse rsp = server.query(query);
 
-  public String escapeQuery(String term) {
-    term = term.replace('?', ' ');
-    term = term.replace('[', ' ');
-    term = term.replace(']', ' ');
-    term = term.replace('/', ' ');
-    term = term.replaceAll("\'", "");
-    return term;
-  }
+		String docText = "";
+		if (rsp.getResults().getNumFound() > 0) {
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			ArrayList<String> results = (ArrayList) rsp.getResults().get(0)
+					.getFieldValues("text");
+			docText = results.get(0);
+		}
+		return docText;
+	}
 
-  public void close() {
-    if (embedded) {
-      ((EmbeddedSolrServer) server).shutdown();
-    }
-  }
+	public String escapeQuery(String term) {
+		term = term.replace('?', ' ');
+		term = term.replace('[', ' ');
+		term = term.replace(']', ' ');
+		term = term.replace('/', ' ');
+		term = term.replaceAll("\'", "");
+		return term;
+	}
+
+	public void close() {
+		if (embedded) {
+			((EmbeddedSolrServer) server).shutdown();
+		}
+	}
 }
