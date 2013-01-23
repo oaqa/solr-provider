55b083d4669e26f52b0943ad3624b98508de8344
alkeshpatel11 <alkeshku@andrew.cmu.edu>
2013-01-23 17:01:13 -0500

    New APIs for indexing added

--- src/main/java/edu/cmu/lti/oaqa/core/provider/solr/SolrWrapper.java
+++ src/main/java/edu/cmu/lti/oaqa/core/provider/solr/SolrWrapper.java
@@ -21,6 +21,9 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrRequest.METHOD;
@@ -28,8 +31,12 @@ import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.request.DirectXmlRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.util.ClientUtils;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.SolrInputField;
 import org.apache.solr.core.CoreContainer;
 
 public final class SolrWrapper implements Closeable {
@@ -40,6 +47,9 @@ public final class SolrWrapper implements Closeable {
 
 	boolean embedded;
 
+	public SolrWrapper(String serverUrl)throws Exception{
+		this.server=createSolrServer(serverUrl);
+	}
 	public SolrWrapper(String serverUrl, Integer serverPort, Boolean embedded,
 			String core) throws Exception {
 		if (embedded != null && embedded.booleanValue()) {
@@ -70,7 +80,7 @@ public final class SolrWrapper implements Closeable {
 
 	private SolrServer createSolrServer(String url) throws Exception {
 		SolrServer server = new HttpSolrServer(url);
-		server.ping();
+		// server.ping();
 		return server;
 	}
 
@@ -98,6 +108,17 @@ public final class SolrWrapper implements Closeable {
 	public SolrDocumentList runQuery(SolrQuery query, int results)
 			throws SolrServerException {
 		QueryResponse rsp = server.query(query);
+		return rsp.getResults();
+	}
+
+	// Added overloaded method for specifying field list
+	public SolrDocumentList runQuery(String q, List<String> fieldList,
+			int results) throws SolrServerException {
+		SolrQuery query = new SolrQuery();
+		query.setQuery(escapeQuery(q));
+		query.setRows(results);
+		query.setFields(fieldList.toArray(new String[1]));
+		QueryResponse rsp = server.query(query, METHOD.POST);
 		return rsp.getResults();
 	}
 
@@ -130,6 +151,71 @@ public final class SolrWrapper implements Closeable {
 	public void close() {
 		if (embedded) {
 			((EmbeddedSolrServer) server).shutdown();
+		}
+	}
+
+	//Building solr document for indexing from key-value pairs
+	public SolrInputDocument buildSolrDocument(HashMap<String, Object> hshMap)
+			throws Exception {
+
+		SolrInputDocument doc = new SolrInputDocument();
+
+		Iterator<String> keys = hshMap.keySet().iterator();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			Object value = hshMap.get(key);
+
+			SolrInputField field = new SolrInputField(key);
+			try {
+
+				doc.addField(field.getName(), value, 1.0f);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return doc;
+
+	}
+	
+	//Converting SolrInputDocument into XML to post over HTTP for Indexing
+	public String convertSolrDocInXML(SolrInputDocument solrDoc)throws Exception{
+		return ClientUtils.toXML(solrDoc);
+	}
+	
+	//Indexing API for document in xml format 
+	public void indexDocument(String docXML) throws Exception {
+
+		String xml = "<add>" + docXML + "</add>";
+		DirectXmlRequest xmlreq = new DirectXmlRequest("/update", xml);
+		server.request(xmlreq);
+	}
+	
+	//Commit indexed documents so that it can be searchable immediately
+	public void indexCommit() throws Exception {
+		server.commit();
+	}
+	
+	//Delete document from index by query
+	public void deleteDocumentByQuery(String query)throws Exception{
+		
+		server.deleteByQuery(query);
+	}
+
+	public static void main(String args[]) {
+		try {
+			SolrWrapper wrapper = new SolrWrapper(
+					"http://localhost:8983/solr/clef-alzheimer-task/", 8983,
+					null, null);
+			HashMap<String,Object>indexMap=new HashMap<String,Object>();
+			indexMap.put("id", "");
+			SolrInputDocument solrInputDoc=wrapper.buildSolrDocument(indexMap);
+			String docXML=wrapper.convertSolrDocInXML(solrInputDoc);
+			wrapper.indexDocument(docXML);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
